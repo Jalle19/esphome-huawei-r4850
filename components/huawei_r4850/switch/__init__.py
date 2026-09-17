877a8dccba6134051783f061854c9e9e7462dc38
Sam Stenvall <sam.stenvall@nitor.com>
2026-09-17 12:27:26 +0300

    Use the built-in restore_mode instead of restore_value for switches
    Makes the standby switch remember the configuration across reboots

--- components/huawei_r4850/switch/__init__.py
+++ components/huawei_r4850/switch/__init__.py
@@ -3,7 +3,7 @@ from esphome.components import switch
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_ID,
-    CONF_RESTORE_VALUE,
+    CONF_RESTORE_MODE,
     ENTITY_CATEGORY_CONFIG,
     ICON_FAN,
     ICON_POWER,
@@ -13,6 +13,18 @@ from .. import CONF_HUAWEI_R4850_ID, HUAWEI_R4850_COMPONENT_SCHEMA, huawei_r4850
 
 CONF_FAN_SPEED_MAX = "fan_speed_max"
 CONF_STANDBY = "standby"
+
+SwitchRestoreMode = cg.esphome_ns.namespace("switch_").enum("SwitchRestoreMode")
+
+RESTORE_MODES = {
+    "RESTORE_DEFAULT_OFF": SwitchRestoreMode.SWITCH_RESTORE_DEFAULT_OFF,
+    "RESTORE_DEFAULT_ON": SwitchRestoreMode.SWITCH_RESTORE_DEFAULT_ON,
+    "ALWAYS_OFF": SwitchRestoreMode.SWITCH_ALWAYS_OFF,
+    "ALWAYS_ON": SwitchRestoreMode.SWITCH_ALWAYS_ON,
+    "RESTORE_INVERTED_DEFAULT_OFF": SwitchRestoreMode.SWITCH_RESTORE_INVERTED_DEFAULT_OFF,
+    "RESTORE_INVERTED_DEFAULT_ON": SwitchRestoreMode.SWITCH_RESTORE_INVERTED_DEFAULT_ON,
+    "DISABLED": SwitchRestoreMode.SWITCH_RESTORE_DISABLED,
+}
 
 HuaweiR4850Switch = huawei_r4850_ns.class_(
     "HuaweiR4850Switch", switch.Switch, cg.Component
@@ -24,14 +36,18 @@ CONFIG_SCHEMA = HUAWEI_R4850_COMPONENT_SCHEMA.extend(
             HuaweiR4850Switch, icon=ICON_FAN, entity_category=ENTITY_CATEGORY_CONFIG
         ).extend(
             {
-                cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
+                cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT_OFF"): cv.enum(
+                    RESTORE_MODES, upper=True, space="_"
+                ),
             }
         ),
         cv.Optional(CONF_STANDBY): switch.switch_schema(
             HuaweiR4850Switch, icon=ICON_POWER, entity_category=ENTITY_CATEGORY_CONFIG
         ).extend(
             {
-                cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
+                cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT_OFF"): cv.enum(
+                    RESTORE_MODES, upper=True, space="_"
+                ),
             }
         ),
     }
@@ -47,7 +63,7 @@ async def to_code(config):
         await switch.register_switch(var, conf)
         cg.add(getattr(hub, "register_input")(var))
         cg.add(var.set_parent(hub, 0x134))
-        cg.add(var.set_restore_value(conf[CONF_RESTORE_VALUE]))
+        cg.add(var.set_restore_mode(conf[CONF_RESTORE_MODE]))
 
     if CONF_STANDBY in config:
         conf = config[CONF_STANDBY]
@@ -56,4 +72,4 @@ async def to_code(config):
         await switch.register_switch(var, conf)
         cg.add(getattr(hub, "register_input")(var))
         cg.add(var.set_parent(hub, 0x132))
-        cg.add(var.set_restore_value(conf[CONF_RESTORE_VALUE]))
+        cg.add(var.set_restore_mode(conf[CONF_RESTORE_MODE]))
